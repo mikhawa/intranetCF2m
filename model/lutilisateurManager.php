a77a3b700c60d53dde306fe5bb13456039a1c244
Stéphane <stephripa@gmail.com>
2019-09-11 13:55:26 +0200

    deconnect

--- model/lutilisateurManager.php
+++ model/lutilisateurManager.php
@@ -122,4 +122,23 @@ class lutilisateurManager
 		return $sqlQuery->fetchAll(PDO::FETCH_ASSOC);
 	}
 
+	//methode de deconnexion
+
+	public function disconnectLutilisateur(){
+
+		$_SESSION = array();
+
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+
+        session_destroy();
+
+        header("Location: ./");
+	}
+
 }
