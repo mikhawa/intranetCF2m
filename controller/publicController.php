8a687f246a6040128b9b139e56fbd11a5961707e
ReinWired <palmisanoandre@hotmail.com>
2019-09-11 14:18:52 +0200

    Updated error_connection

--- controller/publicController.php
+++ controller/publicController.php
@@ -5,21 +5,17 @@
 	
 	if(isset($_POST['lenomutilisateur']) && isset($_POST['lemotdepasse'])) {
 		$utilisateur = new lutilisateur($_POST);
-		if( $connectionStatus = $utilisateurManager->connectLutilisateur($utilisateur) ) {
+		if( $utilisateurManager->connectLutilisateur($utilisateur) ) {
 			header('Location: ./');
 		} else {
-			$error_connection = 'Votre nom d\'utilisateur ou votre login est erroné. Veuillez retenter.';
+			echo $twig->render("public/homepage.html.twig", ["error_connection" => 'Votre nom d\'utilisateur ou votre login est erroné. Veuillez retenter.']);
 		}
-	}
+	} else {
 	
 // page d'accueil
 
     // lien vers la page d'accueil
 	
-    if(isset($error_connection)) {
-		echo $twig->render("public/homepage.html.twig", ["error_connection" => $error_connection]);
-	} else {
-		echo $twig->render("public/homepage.html.twig");
+	echo $twig->render("public/homepage.html.twig");
+
 	}
-
-
