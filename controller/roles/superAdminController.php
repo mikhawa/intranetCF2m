9fcd06b5994436d5478f91ea53c6a46cf5fd1213
mikhawa <michaeljpitz@gmail.com>
2019-09-12 11:23:07 +0200

    chane to superAdminController

--- controller/roles/superAdminController.php
+++ controller/roles/superAdminController.php
@@ -37,9 +37,3 @@ if (isset($_GET['createLinscription'])) {
 
     echo $twig->render('ajoutLinscription.html.twig',['section'=>$section]);
 }
-
-
-// page d'accueil
-
-    // lien vers la page d'accueil
-    echo $twig->render("public/homepage.html.twig");
