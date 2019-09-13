e3016569a5e1474c0e8d06d9241fcdac6c9b5529
Amir <amir.a.webdev@gmail.com>
2019-09-13 10:12:04 +0200

    mise à jour

--- controller/privateController.php
+++ controller/privateController.php
@@ -1,37 +1,11 @@
 <?php
 
-<<<<<<< HEAD
+$utilisateurManager = new lutilisateurManager($db_connect);
+
 if (isset($_GET['deconnect'])) {
-=======
-$utilisateurManager=new lutilisateurManager($db_connect);
 
-if(isset($_GET['deconnect'])){
->>>>>>> d5df8bca2edc6dbb20ec0ad151c8aae6de02a2f5
+    $lutilisateurM->disconnectLutilisateur();
 
-    $lutilisateurM->deconnectLutilisateur();
-
-<<<<<<< HEAD
-} elseif (isset($_GET['stagiaireContorller === 1'])) {
-
-    require_once "../controller/roles/stagiaireContorller.php";
-
-} elseif (isset($_GET['superAdminController === 2'])) {
-
-    require_once "../controller/roles/superAdminController.php";
-
-} elseif (isset($_GET['accueilController === 3'])) {
-
-    require_once "../controller/roles/accueilController.php";
-
-} elseif (isset($_GET['referentController === 4'])) {
-
-    require_once "../controller/roles/referentController.php";
-
-} elseif (isset($_GET['personelController === 5'])) {
-
-    require_once "../controller/roles/personelController.php";
-
-=======
 }
 
 switch ($_SESSION['idlerole']) {
@@ -52,5 +26,4 @@ switch ($_SESSION['idlerole']) {
         break;
     default:
         echo "Session introuvable";
->>>>>>> d5df8bca2edc6dbb20ec0ad151c8aae6de02a2f5
 }
--- controller/roles/personnelController.php
+++ controller/roles/personnelController.php
@@ -1,8 +1,3 @@
 <?php
 
-<<<<<<< HEAD
-    echo $twig->render("public/homepage.html.twig");
-=======
-    
 echo $twig->render("lutilisateur/lutilisateur_afficher_presence.html.twig");
->>>>>>> d5df8bca2edc6dbb20ec0ad151c8aae6de02a2f5
