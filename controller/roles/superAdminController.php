6723d1f5820d5849a4891cda652c8f673ce39b45
ReinWired <palmisanoandre@hotmail.com>
2019-09-16 11:25:38 +0200

    Added lafiliereManager.php, updated lafiliere_afficherlaliste.html.twig and temporary modification to superAdminController.php

--- controller/roles/superAdminController.php
+++ controller/roles/superAdminController.php
@@ -1,5 +1,5 @@
 <?php
-
+	if( isset($_GET['lafiliere']) ) echo $twig->render('lafiliere/lafiliere_afficherliste.html.twig',['session'=>$_SESSION, 'detailfiliere'=>$lafiliereM->selectAllLafiliere()]); else
 // on appelle l'a vue générée par twig l'accueil du superAdmin
     echo $twig->render('roles/admin/admin_homepage.html.twig',['session'=>$_SESSION]);
 
--- model/lafiliereManager.php
+++ model/lafiliereManager.php
@@ -1,17 +1,95 @@
 <?php
 
+class lafiliereManager
+{
+	
+	private $db;
 
+	public function __construct(MyPDO $connect)
+	{
+		 $this->db=$connect;       
+	}
+	
+	public function displayContentLafiliere(): array {
+		$sql = "
+		DESCRIBE
+			lafiliere;";
+		$sqlQuery = $this->db->prepare($sql);
+		$sqlQuery->execute();
+		
+		return $sqlQuery->fetchAll(PDO::FETCH_ASSOC);
+	}
+	
+	public function selectLafiliere(int $id): array {
+		$sql = "
+		SELECT
+			*
+		FROM
+			lafiliere
+		WHERE
+			idlafiliere = :id;";
+		$sqlQuery = $this->db->prepare($sql);
+		$sqlQuery->bindParam(":id", $id, PDO::PARAM_INT);
+		$sqlQuery->execute();
+		
+		return $sqlQuery->fetch(PDO::FETCH_ASSOC);
+	}
+	
+	public function updateLafiliere(int $id, array $datas) {
+		$updateDatas = "";
+		foreach($datas as $dataField => $data) {
+			$updateDatas .= $dataField . " = '" . $data . "', ";
+		}
+		$updateDatas = substr($updateDatas, 0, -2);
+	
+		$sql = "
+		UPDATE
+			lafiliere
+		SET
+			" . $updateDatas . "
+		WHERE
+			idlafiliere = :id;";
+		$sqlQuery = $this->db->prepare($sql);
+		$sqlQuery->bindParam(":id", $id, PDO::PARAM_INT);
+		$sqlQuery->execute();
+	}
+	
+	public function insertLafiliere(array $datas): void {
+		$sql = "
+		INSERT INTO lafiliere(lenom, lacronyme, lacouleur, lepicto)
+		VALUES
+			(";
+		foreach($datas as $data) {
+			$sql .= (gettype($data) == "string" ? "'" . $data . "'" : $data) . ", ";
+		}
+		$sql = substr($sql, 0, -2);
+		$sql .= ");";
+		$sqlQuery = $this->db->prepare($sql);
+		$sqlQuery->execute();
+	}
 
-
-
-class lafiliereManager{
-
-   private $db;
-
-   public function __construct(MyPDO $connect)
-   {
-         $this->db=$connect;       
-   }
-
-
+	public function deleteLafiliere(int $id): void {
+		$sql = "
+		DELETE
+		FROM
+			lafiliere
+		WHERE
+			idlafiliere = :id;";
+		$sqlQuery = $this->db->prepare($sql);
+		$sqlQuery->bindParam(":id", $id, PDO::PARAM_INT);
+		$sqlQuery->execute();
+	}
+	
+	public function selectAllLafiliere(): array {
+		$sql = "
+		SELECT
+			*
+		FROM
+			lafiliere";
+		$sqlQuery = $this->db->prepare($sql);
+		$sqlQuery->execute();
+		
+		return $sqlQuery->fetchAll(PDO::FETCH_ASSOC);
+	}
+	
 }
