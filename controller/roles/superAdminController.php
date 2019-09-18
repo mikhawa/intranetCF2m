d2a85a002c49dc970a725f8dbe3d9fd88dd5ef09
jedafinal <jedafinal@hotmail.com>
2019-09-18 13:42:38 +0200

    filier

--- controller/roles/superAdminController.php
+++ controller/roles/superAdminController.php
@@ -1,5 +1,20 @@
 <?php
-	if( isset($_GET['lafiliere']) ) echo $twig->render('lafiliere/lafiliere_afficherliste.html.twig',['session'=>$_SESSION, 'detailfiliere'=>$lafiliereM->filiereSelectAll()]); else
-// on appelle l'a vue générée par twig l'accueil du superAdmin
+    if( isset($_GET['viewlafiliere'])){
+    
+    echo $twig->render('lafiliere/lafiliere_afficherliste.html.twig',['session'=>$_SESSION, 'detailfiliere'=>$lafiliereM->filiereSelectAll()]); 
+
+    } else if (isset($_GET["updatelafiliere"]) && ctype_digit($_GET["updatelafiliere"])) {
+    echo $twig->render('lafiliere/lafiliere_modifier.html.twig' ,['detail'=>$filierManager]);
+
+
+
+} else {
+
     echo $twig->render('roles/admin/admin_homepage.html.twig',['session'=>$_SESSION]);
+
+
+}
+
+    
+
 
