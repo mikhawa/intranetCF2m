4df19da48f9ce1326e50648a4ee0d8631abe70ce
Stéphane <stephripa@gmail.com>
2019-09-18 13:50:35 +0200

    màj

--- controller/roles/superAdminController.php
+++ controller/roles/superAdminController.php
@@ -1,5 +1,14 @@
 <?php
-	if( isset($_GET['lafiliere']) ) echo $twig->render('lafiliere/lafiliere_afficherliste.html.twig',['session'=>$_SESSION, 'detailfiliere'=>$lafiliereM->filiereSelectAll()]); else
-// on appelle l'a vue générée par twig l'accueil du superAdmin
-    echo $twig->render('roles/admin/admin_homepage.html.twig',['session'=>$_SESSION]);
 
+    if( isset($_GET['viewlafiliere']) ) {
+        echo $twig->render('lafiliere/lafiliere_afficherliste.html.twig',['session'=>$_SESSION, 'detailfiliere'=>$lafiliereM->filiereSelectAll()]); 
+    }
+    else if (isset($_GET['insertlafiliere'])) {
+
+        
+
+        echo $twig->render('lafiliere/lafiliere_ajouter.html.twig');
+    }
+    else { // on appelle la vue générée par twig l'accueil du superAdmin
+        echo $twig->render('roles/admin/admin_homepage.html.twig',['session'=>$_SESSION]);
+    }
