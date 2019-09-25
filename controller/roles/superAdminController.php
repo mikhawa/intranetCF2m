161351994f95b0020c85b8e2957a5e940c3d3b94
mikhawa <michaeljpitz@gmail.com>
2019-09-25 10:14:20 +0200

    change lafiliereManager for update

--- controller/roles/superAdminController.php
+++ controller/roles/superAdminController.php
@@ -1,36 +1,47 @@
 <?php
-
+// view all filieres
 if (isset($_GET['viewlafiliere'])) {
     
     echo $twig->render('lafiliere/lafiliere_afficherliste.html.twig', ['detailfiliere' => $lafiliereM->filiereSelectAll()]);
-    
+
+// insert a filiere    
 } elseif (isset($_GET['insertlafiliere'])) {
 
     if (isset($_POST['lenom'])) {
+        
         $newfiliere = new lafiliere($_POST);
+        
         $insert = $lafiliereM->filiereCreate($newfiliere);
-        header("Location: ./");
+        
+        header("Location: ./?viewlafiliere");
+        exit;
     }
 
     echo $twig->render('lafiliere/lafiliere_ajouter.html.twig');
-    
+
+// delete a filiere    
 } elseif (isset($_GET['deletelafiliere']) && ctype_digit($_GET['deletelafiliere'])) {
 
     $idlafiliere = (int) $_GET['deletelafiliere'];
-
+    
+    // validated delete
     if (isset($_GET['ok'])) {
         
         $lafiliereM->filiereDelete($idlafiliere);
 
-        header("Location: ./");
+        header("Location: ./?viewlafiliere");
         
     } else {
         
         echo $twig->render('lafiliere/lafiliere_delete.html.twig', ['id' => $idlafiliere]);
     }
+
+// update a filiere    
 } else if (isset($_GET["updatelafiliere"]) && ctype_digit($_GET["updatelafiliere"])) {
     
+    // submit updating filiere
     if(isset($_POST['idlafiliere'])){
+        
         $updatelafiliere = new lafiliere($_POST);
         
     }else{
