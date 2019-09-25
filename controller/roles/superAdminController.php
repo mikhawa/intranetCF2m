779117361f6477a7715f36fa357e744701dfda69
mikhawa <michaeljpitz@gmail.com>
2019-09-25 10:01:59 +0200

    link to homepage

--- controller/roles/superAdminController.php
+++ controller/roles/superAdminController.php
@@ -1,42 +1,44 @@
 <?php
 
+if (isset($_GET['viewlafiliere'])) {
+    
+    echo $twig->render('lafiliere/lafiliere_afficherliste.html.twig', ['detailfiliere' => $lafiliereM->filiereSelectAll()]);
+    
+} elseif (isset($_GET['insertlafiliere'])) {
 
-    if( isset($_GET['viewlafiliere']) ) {
-        echo $twig->render('lafiliere/lafiliere_afficherliste.html.twig',['detailfiliere'=>$lafiliereM->filiereSelectAll()]); 
+    if (isset($_POST['lenom'])) {
+        $newfiliere = new lafiliere($_POST);
+        $insert = $lafiliereM->filiereCreate($newfiliere);
+        header("Location: ./");
+    }
 
-    }elseif (isset($_GET['insertlafiliere'])) {
+    echo $twig->render('lafiliere/lafiliere_ajouter.html.twig');
+    
+} elseif (isset($_GET['deletelafiliere']) && ctype_digit($_GET['deletelafiliere'])) {
 
-        if(isset($_POST['lenom'])){
-            $newfiliere = new lafiliere($_POST);
-            $insert = $lafiliereM->filiereCreate($newfiliere);
-            header("Location: ./");
-        }
+    $idlafiliere = (int) $_GET['deletelafiliere'];
 
-        echo $twig->render('lafiliere/lafiliere_ajouter.html.twig');
-
-    }elseif(isset($_GET['deletelafiliere']) && ctype_digit($_GET['deletelafiliere'])) {
+    if (isset($_GET['ok'])) {
         
-        $idlafiliere = (int) $_GET['deletelafiliere'];
+        $lafiliereM->filiereDelete($idlafiliere);
 
-        if (isset($_GET['ok'])){
-            $lafiliereM->filiereDelete($idlafiliere);
-
-            header("Location: ./");
-        }else{
-            echo $twig->render('lafiliere/lafiliere_delete.html.twig',['id'=>$idlafiliere]);
-
-            
-        }
-
-    }else if (isset($_GET["updatelafiliere"]) && ctype_digit($_GET["updatelafiliere"])) {
-    echo $twig->render('lafiliere/lafiliere_modifier.html.twig',['section'=>$lafiliereM->filiereSelectById($_GET['updatelafiliere'])]);
-
-
-
+        header("Location: ./");
+        
+    } else {
+        
+        echo $twig->render('lafiliere/lafiliere_delete.html.twig', ['id' => $idlafiliere]);
+    }
+} else if (isset($_GET["updatelafiliere"]) && ctype_digit($_GET["updatelafiliere"])) {
+    
+    if(isset($_POST['idlafiliere'])){
+        $updatelafiliere = new lafiliere($_POST);
+        
+    }else{
+        
+    echo $twig->render('lafiliere/lafiliere_modifier.html.twig', ['section' => $lafiliereM->filiereSelectById($_GET['updatelafiliere'])]);
+    }   
 } else {
 
-    echo $twig->render('roles/admin/admin_homepage.html.twig',['session'=>$_SESSION]);
-
-
+    echo $twig->render('roles/admin/admin_homepage.html.twig', ['session' => $_SESSION]);
 }
 
