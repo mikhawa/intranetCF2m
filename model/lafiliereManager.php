0f8c770aa7fa164f60ee26f72d523d77f8f60612
mikhawa <michaeljpitz@gmail.com>
2019-10-09 16:24:25 +0200

    upload dans la classe static uploadDoc avec renommagerenameDoc et upload réel avec uploadFichier

--- model/lafiliereManager.php
+++ model/lafiliereManager.php
@@ -72,47 +72,7 @@ class lafiliereManager {
     
     }
 
-        function uploadFichier(array $datas){
-
-            
-    
-            $dossier = 'img/upload/';
-            $fichier = basename($datas['name']);
-            $taille_maxi = 1000000;
-            $taille = filesize($datas['tmp_name']);
-            $extensions = array('.png', '.gif', '.jpg', '.jpeg');
-            $extension = strrchr($datas['name'], '.'); 
-            //Début des vérifications de sécurité...
-            if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
-            {
-                $erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
-            }
-            if($taille>$taille_maxi)
-            {
-                $erreur = 'Le fichier est trop gros...';
-            }
-            if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
-            {
-                //On formate le nom du fichier ici...
-                $fichier = strtr($fichier, 
-                    'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
-                    'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
-                $fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
-                if(move_uploaded_file($datas['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
-                {
-                    echo 'Upload effectué avec succès !';
-                }
-                else //Sinon (la fonction renvoie FALSE).
-                {
-                    echo 'Echec de l\'upload !';
-                }
-            }
-            else
-            {
-                echo $erreur;
-            }
-    
-        }
+        
     
 
     public function filiereUpdate(lafiliere $datas, int $get) {
