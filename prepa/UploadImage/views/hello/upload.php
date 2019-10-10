954b736e5213fa34be5f9a93852a5ee5e6ed385b
gmdevweb <gmdevweb@gmail.com>
2019-10-10 16:21:58 +0200

    maj

---
+++ prepa/UploadImage/views/hello/upload.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form method="post" action="multi_file_upload" enctype="multipart/form-data">
+<input type="file" name="image[]" multiple="multiple" >
+<p align="center"><button type="submit" class="btn btn-warning" id="butsave" name="submit">Submit<span class="glyphicon glyphicon-send"></span></button></p>
+</form>
+</body>
+</html>
