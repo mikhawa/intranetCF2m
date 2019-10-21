3e7742475e76f8628f1fa99dbc9fcadf719b06d0
Stéphane <stephripa@gmail.com>
2019-10-21 16:27:16 +0200

    màj

--- controller/roles/pedagogiqueController.php
+++ controller/roles/pedagogiqueController.php
@@ -1,5 +1,18 @@
 <?php
 
+if(isset($_GET['viewdetailsession'])) {
+
+    $selectEval = $evaluationM->selectAllFiliereForEval();
+
+
+     echo $twig->render("view_stagiaires/choix_filiere.html.twig", ['lutilisateur'=>$selectEval]);
+
+
+
+
+}
+else{
+
 if(!isset($_SESSION['bandeau'])){
     $pourEntree = true;
     $_SESSION['bandeau']=true;
@@ -7,3 +20,4 @@ if(!isset($_SESSION['bandeau'])){
     $pourEntree = false;
 }
 echo $twig->render('roles/pedagogique/pedagogique_homepage.html.twig', ['entree' => $pourEntree,"session"=>$_SESSION]);
+}
---
+++ model/evaluationManager.php
@@ -0,0 +1,48 @@
+<?php
+
+class evaluationManager {
+    private $db;
+    public function __construct(MyPDO $connect) {
+        $this->db = $connect;
+    }
+
+
+    //selection de tous les stagiaires
+
+    public function selectAllStagiairesForEval(){
+
+        $sql = "SELECT u.lenom, u.leprenom
+		FROM lutilisateur u
+		INNER JOIN linscription i
+		ON i.utilisateur_idutilisateur = u.idlutilisateur
+		INNER JOIN lasession s
+        ON s.idlasession = i.lasession_idsession;";
+        
+        $recup = $this->db->query($sql);
+        if ($recup->rowCount() === 0) {
+            return [];
+        }
+        return $recup->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function selectAllFiliereForEval(): array{
+
+        $sql="SELECT lenom
+              FROM lafiliere";
+
+        $recup = $this->db->query($sql);
+        if ($recup->rowCount() === 0) {
+            return [];
+        }
+        return $recup->fetchAll(PDO::FETCH_ASSOC);  
+
+    }
+
+
+
+
+
+
+
+
+}
