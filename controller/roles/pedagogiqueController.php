104a17c9731d223b24b1b15f3c4c2ec5e977dbca
Stéphane <stephripa@gmail.com>
2019-10-24 14:30:24 +0200

    màj

--- controller/roles/pedagogiqueController.php
+++ controller/roles/pedagogiqueController.php
@@ -1,5 +1,5 @@
 <?php
-
+//choix des filières
 if(isset($_GET['viewdetailsession'])) {
 
     $selectEval = $evaluationM->selectAllFiliereForEval();
@@ -9,7 +9,7 @@ if(isset($_GET['viewdetailsession'])) {
 
 
 
-
+//choix des stagiaires de la filiere choisie
 }elseif(isset($_GET['choixFiliere']) && ctype_digit($_GET['choixFiliere'])){
     $choixFiliere = (int)$_GET['choixFiliere'];
 
@@ -18,18 +18,39 @@ if(isset($_GET['viewdetailsession'])) {
     echo $twig->render("view_stagiaires/choix_stagiaire.html.twig",['lutilisateur'=>$nomStagiaire]);
 
 
-
+// choix du stagiaire pour voir son profil
  }elseif(isset($_GET['profil']) && ctype_digit($_GET['profil'])){
 
            $profil=(int)$_GET['profil'];
 
     
 
-            $profilStagiaire = $evaluationM-> selectProfilStagiaire($profil);
+            $profilStagiaire = $evaluationM->selectProfilStagiaire($profil);
 
-            echo $twig->render('view_stagiaires/profil_stagiaire.html.twig',['leprofil'=>$profilStagiaire]);
+            echo $twig->render('view_stagiaires/profil_stagiaire.html.twig',['idlutilisateur'=>$profilStagiaire]);
 
-        }
+
+//update des infos du stagiaire
+}elseif(isset($_GET['ajoutInfo']) && ctype_digit($_GET['ajoutInfo'])){
+
+    if(isset($_POST['idlutilisateur'])){
+
+    $updateStagiaire = new evaluation($_POST);
+
+    $evaluationM->updateStagiaire($updateStagiaire, $_GET['ajoutInfo']);
+
+    header("Location: ./?ajoutInfo");
+    var_dump($_POST);
+    }else{
+
+        echo $twig->render('view_stagiaires/modifier_stagiaire.html.twig',['section'=>$evaluationM->selectProfilStagiaire($_GET['ajoutInfo'])]);
+    }
+
+
+
+
+
+}
           
 
 
--- model/evaluationManager.php
+++ model/evaluationManager.php
@@ -11,7 +11,7 @@ class evaluationManager {
 
     public function selectAllStagiairesForEval(int $idlafiliere): array{
 
-        $sql = "SELECT u.lenom, u.leprenom
+        $sql = "SELECT u.lenom, u.leprenom, u.idlutilisateur
 		FROM lutilisateur u
 		INNER JOIN linscription i
 		ON i.utilisateur_idutilisateur = u.idlutilisateur
@@ -43,16 +43,22 @@ class evaluationManager {
 
     }
 
-
+     //selection du stagiaire By id
     public function selectProfilStagiaire(int $idlutilisateur): array{
 
-        $sql="SELECT u.lenom, u.leprenom, u.lemail, f.lenom, s.lenom
-        FROM lutilisateur u 
-        INNER JOIN lafiliere f 
-        ON f.idlafiliere = u.idlutilisateur
-        INNER JOIN lasession s 
+        $sql="SELECT (u.lenom) AS nomFamille, u.leprenom, u.lemail, (f.lenom) AS filiereName, (s.lenom) AS sessionName, u.lenomutilisateur, u.idlutilisateur, r.lintitule
+        FROM lutilisateur u
+        INNER JOIN lutilisateur_has_lerole h
+        ON h.lutilisateur_idutilisateur = u.idlutilisateur
+        INNER JOIN lerole r 
+        ON r.idlerole = h.lerole_idlerole
+        LEFT JOIN lafiliere f 
+        ON f.idlafiliere = r.idlerole
+        LEFT JOIN lasession s 
         ON s.idlasession = u.idlutilisateur
-        WHERE u.idlutilisateur =?";
+        LEFT JOIN linscription l 
+        ON l.utilisateur_idutilisateur = u.idlutilisateur
+        WHERE u.idlutilisateur = ?;";
 
         $recup = $this->db->prepare($sql);
         $recup->bindValue(1,$idlutilisateur,PDO::PARAM_INT);
@@ -66,6 +72,40 @@ class evaluationManager {
 
 
     }
+
+    //update d'un stagiaire
+    public function updateStagiaire(evaluation $update) {
+        if ( empty($update->getLenom()) || empty($update->getLeprenom()) || empty($update->getLemail()) || empty($update->getLenom())
+             || empty($update->getLenomutilisateur()) ||  empty($update->getIdlutilisateur()) ) {
+            return false;
+    	}
+
+         $sql ="UPDATE lutilisateur, lerole, lafiliere, lasession  
+         SET lutilisateur.Lenom=?, Leprenom=?, Lemail=?, lafiliere.lenom=?, Lenomutilisateur=?
+         WHERE idlutilisateur=?";
+
+
+        $updateLeStagiaire = $this->db->prepare($sql);
+        $updateLeStagiaire->bindValue(1, $update->getLenom(), PDO::PARAM_STR);
+        $updateLeStagiaire->bindValue(2, $update->getLeprenom(), PDO::PARAM_STR);
+        $updateLeStagiaire->bindValue(3, $update->getLemail(), PDO::PARAM_STR);
+        $updateLeStagiaire->bindValue(4, $update->getLenom(), PDO::PARAM_STR);
+        $updateLeStagiaire->bindValue(5, $update->getLenomutilisateur(), PDO::PARAM_STR);
+        $updateLeStagiaire->bindValue(6, $update->getIdlutilisateur(), PDO::PARAM_INT);
+
+
+        try{
+
+            $update->execute();
+            return true;
+
+        } catch(PDOException $e){
+
+            echo '<h2 style="color: red;">ERROR: ' . $e->getMessage() . '</h2>';
+            return false;
+
+        }
+	}
 
 
 
