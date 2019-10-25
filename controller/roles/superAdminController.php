90a918e34af50e99affa7a6929407770fd436fcd
jeanbapt56 <toutafela@gmail.com>
2019-10-25 12:05:46 +0200

    linscription

--- controller/roles/superAdminController.php
+++ controller/roles/superAdminController.php
@@ -217,14 +217,37 @@ elseif (isset($_GET['insertleconge']))
     }
   
   
-//inscription
-  
+//linscription
 }elseif (isset($_GET["viewlinscription"])) {
     echo $twig->render("linscription/linscription_afficherliste.html.twig", ['detailinscription' => $linscriptionM->selectAllLinscription()]);
+
 }elseif (isset($_GET["ajouterlinscription"])) {
+    if(!empty($_POST)){
+        $newlinscription = new linscription($_POST);
+        s($_POST,$newlinscription);
+        // insertion
+        $insert=$linscriptionM->linscriptionCreate($newlinscription);
+        }
+
+else{
     echo $twig->render("linscription/linscription_ajouter.html.twig", ['detailUsers' => $lutilisateurM->lutilisateurSelectAll(), 'detailSession' => $lasessionM->sessionSelectALL()]);
+}
+
+//update linscription
 }elseif (isset($_GET["updatelinscription"])) {
     echo $twig->render("linscription/linscription_modifier.html.twig", ['modifutilisateur' => $lutilisateurM->lutilisateurSelectAll(), 'modifutilisateur' => $lasessionM->sessionSelectALL()]);
+
+//delete linscription
+}elseif(isset($_GET['deletelinscription']) && ctype_digit($_GET['deletelinscription'])){
+    $idDeletelinscription = (int)$_GET['deletelinscription'];
+    if(isset($_GET['ok'])){
+        $linscriptionM->deletelinscription($idDeletelinscription);
+      
+      
+      
+      }else{
+    echo $twig->render('lerole/lerole_delete.html.twig',['id'=>$idDeleteRole]);
+    }
     
 // Display views for sessions
 } elseif (isset($_GET['viewlasession'])) {
